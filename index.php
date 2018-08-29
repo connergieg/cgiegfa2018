8d325669910d2f36d48ecdeba076ead4ac259446
Conner Gieg <connergieg@hotmail.com>
2018-08-29 22:04:43 +0000

    lab1

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-Welcome to Conner Gieg's CST336 Home Page!
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title> Conner Gieg </title>
+    </head>
+    
+    <body>
+        <?php
+            echo "<h1> Conner Gieg's 336 site </h1>";
+        ?>
+    </body>
+</html>
