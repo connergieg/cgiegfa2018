4295991b93557668eb3df1baf0c1366f465137c0
Conner Gieg <connergieg@hotmail.com>
2018-11-02 15:44:17 +0000

    debugging

--- labs/lab7/loginProcess.php
+++ labs/lab7/loginProcess.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include "inc/functions.php";
+    // include "inc/functions.php";
     $_SESSION["username"] = $_POST["username"];
     $_SESSION["password"] = $_POST["password"];
     
